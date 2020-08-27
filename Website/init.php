a2338f68f06be860590e1aed4bbef11ae9fd0867
HoussamMrabet <dodslim100@gmail.com>
2020-08-27 05:07:30 +0100

    add initialise file

---
+++ Website/init.php
@@ -0,0 +1,33 @@
+<?php
+
+	// Error Reporting
+
+	ini_set('display_errors', 'On');
+	error_reporting(E_ALL);
+
+	include 'admin/connect.php';
+
+	$sessionUser = '';
+	$sessionAvatar = '';
+	
+	if (isset($_SESSION['user'])) {
+		$sessionUser = $_SESSION['user'];
+		$sessionAvatar = $_SESSION['avatar'];
+	}
+
+	// Routes
+
+	$tpl 	= 'includes/templates/'; // Template Directory
+	$lang 	= 'includes/languages/'; // Language Directory
+	$func	= 'includes/functions/'; // Functions Directory
+	$css 	= 'layout/css/'; // Css Directory
+	$js 	= 'layout/js/'; // Js Directory
+
+	// Include The Important Files
+
+	include $func . 'functions.php';
+	include $lang . 'english.php';
+	include $tpl . 'header.php';
+	
+
+	
