b7e699d8711e3dbfa8a172e9a98ab93fa46d8e19
HoussamMrabet <dodslim100@gmail.com>
2020-08-28 19:25:14 +0100

    Update the comments manage page for admin

--- Website/admin/comments.php
+++ Website/admin/comments.php
@@ -57,7 +57,6 @@
 				<div class="table-responsive">
 					<table class="main-table text-center table table-bordered">
 						<tr>
-							<td>ID</td>
 							<td>Feedback</td>
 							<td>Item Name</td>
 							<td>User Name</td>
@@ -67,13 +66,11 @@
 						<?php
 							foreach($comments as $comment) {
 								echo "<tr>";
-									echo "<td>" . $comment['c_id'] . "</td>";
 									echo "<td>" . $comment['comment'] . "</td>";
 									echo "<td>" . $comment['Item_Name'] . "</td>";
 									echo "<td>" . $comment['Member'] . "</td>";
 									echo "<td>" . $comment['comment_date'] ."</td>";
 									echo "<td>
-										<a href='comments.php?do=Edit&comid=" . $comment['c_id'] . "' class='btn btn-success'><i class='fa fa-edit'></i> Edit</a>
 										<a href='comments.php?do=Delete&comid=" . $comment['c_id'] . "' class='btn btn-danger confirm'><i class='fa fa-close'></i> Delete </a>";
 										if ($comment['status'] == 0) {
 											echo "<a href='comments.php?do=Approve&comid="
@@ -99,104 +96,6 @@
 			} ?>
 
 		<?php 
-
-		} elseif ($do == 'Edit') {
-
-			// Check If Get Request comid Is Numeric & Get Its Integer Value
-
-			$comid = isset($_GET['comid']) && is_numeric($_GET['comid']) ? intval($_GET['comid']) : 0;
-
-			// Select All Data Depend On This ID
-
-			$stmt = $con->prepare("SELECT * FROM comments WHERE c_id = ?");
-
-			// Execute Query
-
-			$stmt->execute(array($comid));
-
-			// Fetch The Data
-
-			$row = $stmt->fetch();
-
-			// The Row Count
-
-			$count = $stmt->rowCount();
-
-			// If There's Such ID Show The Form
-
-			if ($count > 0) { ?>
-
-				<h1 class="text-center">Edit Comment</h1>
-				<div class="container">
-					<form class="form-horizontal" action="?do=Update" method="POST">
-						<input type="hidden" name="comid" value="<?php echo $comid ?>" />
-						<!-- Start Comment Field -->
-						<div class="form-group form-group-lg">
-							<label class="col-sm-2 control-label">Comment</label>
-							<div class="col-sm-10 col-md-6">
-								<textarea class="form-control" name="comment"><?php echo $row['comment'] ?></textarea>
-							</div>
-						</div>
-						<!-- End Comment Field -->
-						<!-- Start Submit Field -->
-						<div class="form-group form-group-lg">
-							<div class="col-sm-offset-2 col-sm-10">
-								<input type="submit" value="Save" class="btn btn-primary btn-sm" />
-							</div>
-						</div>
-						<!-- End Submit Field -->
-					</form>
-				</div>
-
-			<?php
-
-			// If There's No Such ID Show Error Message
-
-			} else {
-
-				echo "<div class='container'>";
-
-				$theMsg = '<div class="alert alert-danger">Theres No Such ID</div>';
-
-				redirectHome($theMsg);
-
-				echo "</div>";
-
-			}
-
-		} elseif ($do == 'Update') { // Update Page
-
-			echo "<h1 class='text-center'>Update Comment</h1>";
-			echo "<div class='container'>";
-
-			if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-
-				// Get Variables From The Form
-
-				$comid 		= $_POST['comid'];
-				$comment 	= $_POST['comment'];
-
-				// Update The Database With This Info
-
-				$stmt = $con->prepare("UPDATE comments SET comment = ? WHERE c_id = ?");
-
-				$stmt->execute(array($comment, $comid));
-
-				// Echo Success Message
-
-				$theMsg = "<div class='alert alert-success'>" . $stmt->rowCount() . ' Record Updated</div>';
-
-				redirectHome($theMsg, 'back');
-
-			} else {
-
-				$theMsg = '<div class="alert alert-danger">Sorry You Cant Browse This Page Directly</div>';
-
-				redirectHome($theMsg);
-
-			}
-
-			echo "</div>";
 
 		} elseif ($do == 'Delete') { // Delete Page
 
